28d54256632fde99a88e6911ff68bc3c564becef
georges.paez <georgesp610@gmail.com>
2024-04-16 16:10:53 -0500

    Proyecto Base

---
+++ src/main/java/co/ucentral/Maicol1213/Miapp/MiappApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.Maicol1213.Miapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentral/Maicol1213/Miapp/MiappApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.Maicol1213.Miapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
